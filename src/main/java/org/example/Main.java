4d21df2ce2dfb1c1573a573652a85b9e3cd6c415
Andre Luckyta Firdaus <andreluckytafirdaus2001@gmail.com>
2023-10-27 10:45:48 +0700

    POST TEST DAY 4

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,21 +4,61 @@ package org.example;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        int a,b,c,d,e;
-        a = 5;
-        b = 2;
-        c = a++ * ++b;
-        System.out.println("Isi varoabel A "+a);
-        System.out.println("Isi varoabel B "+b);
-        d = a + b + c;
-        e = ( c + d ) * a - b;
-        System.out.println("Main.main " + "Isi Variabel A " + a);
-        System.out.println("Isi varoabel E "+e);
-        System.out.println("Isi varoabel A "+a);
-        System.out.println("Isi varoabel B "+b);
-        System.out.println("Isi varoabel C "+c);
-        System.out.println("Isi varoabel D "+d);
-        System.out.printf("Hello and welcome!");
+        /*String str1 = "Tessy Wahyuni";
+
+        String str2 = "Riwayati";
+
+        String str3 = str1 + str2 + "Hartati";
+        System.out.println(str3);*/
+        //System.out.println("Tessy".concat("Wahyuni".concat("Riwayati".concat("Hartati"))));
+
+/*        String jampe = "Hong wilaheng sekaring bawono langgeng";
+
+        System.out.println(jampe.length());*/
+
+       /* String tahun = "1728";
+        System.out.println(tahun.length());
+        */
+
+
+      /*  String profesi = "paranormal";
+        System.out.println(profesi.charAt(5));*/
+
+
+        /*String nama="Bolot Sulaiman";
+        System.out.println(nama.substring(10,14));*/
+
+       /* String nama1 = "Sutarno";
+
+        String nama2 = "sutarno";
+
+        boolean persamaan = nama2.equalsIgnoreCase(nama1);*/
+    /*    System.out.println(persamaan);*/
+
+//        String nama1 = "Sutarno";
+//
+//        String nama2 = "sutarno";
+//
+//        boolean persamaan = nama1.equalsIgnoreCase(nama2);
+//        System.out.println(persamaan);
+
+
+
+/*
+        String nama1 = "Sutarno";
+
+        String nama2 = "Sutarno";
+
+        boolean persamaan = nama1.equals(nama2);
+        System.out.println(persamaan);
+*/
+
+        String nama1 = "Sutarno";
+
+        String nama2 = "sutarno";
+
+        boolean persamaan = nama1.equals(nama2);
+        System.out.println(persamaan);
 
     }
 }
